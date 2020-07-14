9551fde3a7ecbc0e20aee03ba82113a295390b6f
quinqu <quinruby0@gmail.com>
2020-07-14 09:59:37 -0700

    new version of app

---
+++ tempo/HeightViewController.swift
@@ -0,0 +1,59 @@
+//
+//  HeightViewController.swift
+//  tempo
+//
+//  Created by Quinn Quintero on 7/13/20.
+//  Copyright © 2020 Quinn Quintero. All rights reserved.
+//
+
+import UIKit
+
+class HeightViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    var pickerData = ["60", "61", "62","63", "64", "65", "66", "67", "68","69", "70", "71", "72", "73", "74"]
+
+
+
+    @IBOutlet weak var picker: UIPickerView!
+    
+    
+    override func viewDidLoad() {
+        print("hello")
+        super.viewDidLoad()
+        picker.delegate = self
+        picker.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        heightLabel.text = pickerData[row]
+    }
+
+//    
+//    @IBAction func heightNextButtonPressed(_ sender: UIButton) {
+//        performSegue(withIdentifier: "tohomepage", sender: self)
+//    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tempo/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  tempo
+//
+//  Created by Quinn Quintero on 7/13/20.
+//  Copyright © 2020 Quinn Quintero. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+          
+      }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        print("here")
+        let vc = SFSafariViewController(url: URL(string: "http://localhost:3000/auth/spotify")!)
+        present(vc, animated: true)
+        
+        vc.loadViewIfNeeded()
+        
+
+//        //if statement
+//        vc.navigationController?.popViewController(animated: true)
+//        vc.dismiss(animated: true, completion: nil)
+        
+//        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+//
+//        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "height_vc") as! HeightViewController
+//        self.present(nextViewController, animated:true, completion:nil)
+    }
+    
+    
+    
+    
+}
+
