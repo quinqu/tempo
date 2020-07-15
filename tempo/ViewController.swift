ff88b1823d8ace53615572a44845815d6bb42fbc
quinqu <quinruby0@gmail.com>
2020-07-14 20:51:42 -0700

    close sfsafariview after login

--- tempo/ViewController.swift
+++ tempo/ViewController.swift
@@ -31,35 +31,13 @@ class ViewController: UIViewController, SFSafariViewControllerDelegate {
         if let sceneDelegate = self.view.window?.windowScene?.delegate as? SceneDelegate {
             sceneDelegate.safariVC = vc
         }
-         
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "height_vc") as! HeightViewController
+        self.present(nextViewController, animated:true, completion:nil)
         
-        
-//        print("hi")
-        
-        
-//        vc.dismiss(animated: true, completion: nil)
-//
-//        vc.loadViewIfNeeded()
-//        //if statement
-//        vc.navigationController?.popViewController(animated: true)
-//        vc.dismiss(animated: true, completion: nil)
-        
-//        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-//
-//        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "height_vc") as! HeightViewController
-        //self.present(nextViewController, animated:true, completion:nil)
-//        AF.request("http://localhost:3000/auth/spotify").validate().responseDecodable(of: Response.self) { response in
-//            debugPrint(response)
-//            let json = JSON(response.result.value)
-            
-//            if response.contains("/login/success") {
-//
-//            }
-            
-//            if response.exists() {
-//                print("response someKey exists")
-//            }
         }
+    
+    
         
     }
     
