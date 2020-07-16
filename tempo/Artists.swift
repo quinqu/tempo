86599e2f8ce46739545fcf8f934805436982ae6f
quinqu <quinruby0@gmail.com>
2020-07-16 14:12:19 -0700

    created structs

--- tempo/Artists.swift
+++ tempo/Artists.swift
@@ -1,13 +1,14 @@
 //
 //  Artists.swift
-//  
+//  tempo
 //
 //  Created by Quinn Quintero on 7/16/20.
+//  Copyright © 2020 Quinn Quintero. All rights reserved.
 //
 
 import Foundation
 
 
-struct Artists: Codable {
-    var name:String = ""
+struct Artists : Codable {
+    var name: String = "" 
 }
