18a71e26f91926f5f84bb41138927d670d079166
quinqu <quinruby0@gmail.com>
2020-07-17 13:48:40 -0700

    added images from albums

--- tempo/PlaylistMadeViewController.swift
+++ tempo/PlaylistMadeViewController.swift
@@ -7,18 +7,44 @@
 //
 
 import UIKit
+import Kingfisher
+
 
 class PlaylistMadeViewController: UIViewController {
-    
     var songs = [Song]()
-    
+    @IBOutlet var tableView: UITableView!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
 
+}
+
+extension PlaylistMadeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("idk")
+    }
+}
+extension PlaylistMadeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return songs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = songs[indexPath.row].name
+        cell.detailTextLabel?.text = songs[indexPath.row].artists[0].name
+        
+        let imageURL = URL(string: songs[indexPath.row].album.images[0].url)
+        cell.imageView?.kf.setImage(with: imageURL) 
+  
+
+        return cell
+    }
 }
