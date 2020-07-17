979d0387fa2069ed42a9084e475369727a1ce46e
quinqu <quinruby0@gmail.com>
2020-07-17 14:51:57 -0700

    cells will not load without images

--- tempo/PlaylistMadeViewController.swift
+++ tempo/PlaylistMadeViewController.swift
@@ -33,11 +33,26 @@ extension PlaylistMadeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = songs[indexPath.row].name
-        //cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.textColor = UIColor.white
         cell.detailTextLabel?.text = songs[indexPath.row].artists[0].name
-        //cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.textColor = UIColor.white
         let imageURL = URL(string: songs[indexPath.row].album.images[0].url)
-        cell.imageView?.kf.setImage(with: imageURL) 
+        cell.imageView?.kf.setImage(with: imageURL) { result in
+            cell.setNeedsLayout() //invalidate current layout
+            cell.layoutIfNeeded() //update immediately
+          switch result {
+          case .success(let value):
+              print("Image: \(value.image). Got from: \(value.cacheType)")
+          case .failure(let error):
+              print("Error: \(error)")
+          }
+        }
+//        ge(with: imageURL, placeholder: nil, options: nil, progressBlock: nil, completionHandler: { (image, error, cacheType, URL) in
+//            cell.setNeedsLayout()
+//        })
+//
+   
+        //cell.imageView?.kf.setImage(with: imageURL)
   
 
         return cell
